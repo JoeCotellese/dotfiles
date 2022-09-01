342bd7168c46cd5a196a0588595dbd94a26d6ae9
Joe Cotellese <joe@cotellese.net>
2022-09-01 10:27:33 -0400

    update xcode aliases

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,2 +1,3 @@
-alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
-alias watchos="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator\ \(Watch\).app"
+export XCPATH="/Volumes/Home/Applications"
+alias ios="open $XCPATH/Xcode.app/Contents/Developer/Applications/Simulator.app"
+alias watchos="open $XCPATH/Xcode.app/Contents/Developer/Applications/Simulator\ \(Watch\).app"
