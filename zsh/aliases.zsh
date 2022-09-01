4c1cf5369b8b86b3418c90b64e7f93e4e65a95f0
Joe Cotellese <joe@cotellese.net>
2022-09-01 10:21:29 -0400

    remove ruby, add pyenv

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,7 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# quickly get to ClipDish related project directories
+alias extractor="cd ~/git-recipeapp/extractor-lambda"
+alias iosapp="cd ~/git-recipeapp/clipdish-ios"
