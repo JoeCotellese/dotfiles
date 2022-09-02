bccf7922c3aed47a352b13cc23d47be8e4dba9b9
Joe Cotellese <joe@cotellese.net>
2022-09-02 07:52:11 -0400

    add an alias for launching mame

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,4 @@ alias cls='clear' # Good 'ol Clear Screen command
 # quickly get to ClipDish related project directories
 alias extractor="cd ~/git-recipeapp/extractor-lambda"
 alias iosapp="cd ~/git-recipeapp/clipdish-ios"
+alias mame="cd /Volumes/External/MAME;mame"
