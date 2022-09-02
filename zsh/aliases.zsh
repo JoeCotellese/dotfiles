8794f6ef8a449f786e9ba986c2c2feb98548d2a1
Joe Cotellese <joe@cotellese.net>
2022-09-02 08:05:21 -0400

    add some mac mini aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,4 +5,13 @@ alias cls='clear' # Good 'ol Clear Screen command
 # quickly get to ClipDish related project directories
 alias extractor="cd ~/git-recipeapp/extractor-lambda"
 alias iosapp="cd ~/git-recipeapp/clipdish-ios"
-alias mame="cd /Volumes/External/MAME;mame"
+
+# Mac mini specific aliases
+if [[ $HOST == "Joes-Mac-mini-2.local" ]]
+  then
+    alias mame="cd /Volumes/External/MAME;mame"
+    alias external="cd /Volumes/External"
+    alias home="cd /Volumes/Home"
+  fi
+
+alias download="cd $HOME/Downloads"
