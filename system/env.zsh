77e0ab644fec45a1cd2074a2b018d3809f490785
Joe Cotellese <joe@cotellese.net>
2024-03-15 08:28:24 -0400

    remove editor & visual

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,10 @@
-export EDITOR=vim
-export VISUAL="$EDITOR"
+# TODO: If I set the editor to VIM
+# then the Ctrl+a and Ctrl+e don't work in tmux
+
+# export EDITOR=vim
+# export VISUAL="$EDITOR"
+# if EDITOR=vi then keybindings are vi-like
+# I don't want that everywhere so force them 
+# back to emails
+# set -g mode-keys emacs
+# set -g status-keys emacs
